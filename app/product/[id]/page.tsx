00fe5add0204b3233ad1d8847b5513c220fb3e85
AlexandreDresch <connectWithAlexandre@proton.me>
2024-05-04 00:10:33 -0300

    feat: add Product page

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { db } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./components/product-image";
+import ProductDetails from "./components/product-details";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+export default async function ProductsPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await db.product.findUnique({
+    where: { id },
+    include: { restaurant: true },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  const complementaryProducts = await db.product.findMany({
+    where: { restaurant: { id: product.restaurant.id } },
+    take: 5,
+    include: { restaurant: true },
+  });
+
+  return (
+    <div>
+      <ProductImage product={product} />
+
+      <ProductDetails
+        product={product}
+        complementaryProducts={complementaryProducts}
+      />
+    </div>
+  );
+}
