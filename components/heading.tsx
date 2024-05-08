b295fafcb34dec2ed165fa6a738c0ff5d72c1206
AlexandreDresch <connectWithAlexandre@proton.me>
2024-05-07 22:52:29 -0300

    feat: add no link option & add className prop to Heading component

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,16 +1,23 @@
 import { ChevronRightIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface HeadingProps {
   title: string;
-  link: string;
+  className?: string;
+  link?: string;
 }
 
-export default function Heading({ title, link }: HeadingProps) {
-  return (
-    <div className="flex items-center justify-between px-5">
-      <h2 className="font-semibold">{title}</h2>
+export default function Heading({ title, link, className }: HeadingProps) {
+  const hasLink = !!link;
 
+  const content = hasLink ? (
+    <Link
+      href={link}
+      passHref
+      className={`flex items-center justify-between ${className}`}
+    >
+      <h2 className="font-semibold">{title}</h2>
       <Button
         variant="ghost"
         className="h-fit p-0 text-primary hover:bg-transparent"
@@ -18,6 +25,12 @@ export default function Heading({ title, link }: HeadingProps) {
         See all
         <ChevronRightIcon size={16} />
       </Button>
+    </Link>
+  ) : (
+    <div className={`flex items-center justify-between ${className}`}>
+      <h2 className="font-semibold">{title}</h2>
     </div>
   );
+
+  return content;
 }
