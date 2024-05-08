ff208b5d94f32f423e50948bd5bbc9053cbf098e
AlexandreDresch <connectWithAlexandre@proton.me>
2024-05-07 22:50:21 -0300

    feat: add home link to Header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { AlignRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.svg" alt="Foodie" height={30} width={100} priority />
+      <Link href="/">
+        <Image src="/logo.svg" alt="Foodie" height={30} width={100} priority />
+      </Link>
 
       <Button
         size="icon"
