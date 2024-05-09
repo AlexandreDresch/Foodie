d8651a8997701fd943e47cbc5873255cf9365f87
AlexandreDresch <connectWithAlexandre@proton.me>
2024-05-08 23:12:23 -0300

    feat: add react-hook-form

--- components/search.tsx
+++ components/search.tsx
@@ -1,15 +1,41 @@
+"use client";
+
 import { SearchIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+interface FormInputs {
+  search: string;
+}
 
 export default function Search() {
-  return (
-    <div className="flex gap-2">
-      <Input placeholder="Search Restaurants" className="border-none" />
+  const router = useRouter();
+  const { register, handleSubmit, setValue } = useForm<FormInputs>();
 
-      <Button size="icon">
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    const { search } = data;
+
+    if (!search) {
+      return;
+    }
+
+    router.push(`/restaurant?search=${search}`);
+  };
+
+  return (
+    <form className="flex gap-2" onSubmit={handleSubmit(onSubmit)}>
+      <Input
+        placeholder="Search Restaurants"
+        className="border-none"
+        {...register("search")}
+        onChange={(e) => setValue("search", e.target.value)}
+      />
+
+      <Button size="icon" type="submit">
         <SearchIcon size={18} />
       </Button>
-    </div>
+    </form>
   );
 }
