81890488f9bf4a78e2e053b0ba518019df5748c8
AlexandreDresch <connectWithAlexandre@proton.me>
2024-05-08 22:22:29 -0300

    refactor: moved Link inside button

--- components/heading.tsx
+++ components/heading.tsx
@@ -11,26 +11,22 @@ interface HeadingProps {
 export default function Heading({ title, link, className }: HeadingProps) {
   const hasLink = !!link;
 
-  const content = hasLink ? (
-    <Link
-      href={link}
-      passHref
-      className={`flex items-center justify-between ${className}`}
-    >
-      <h2 className="font-semibold">{title}</h2>
-      <Button
-        variant="ghost"
-        className="h-fit p-0 text-primary hover:bg-transparent"
-      >
-        See all
-        <ChevronRightIcon size={16} />
-      </Button>
-    </Link>
-  ) : (
+  return (
     <div className={`flex items-center justify-between ${className}`}>
       <h2 className="font-semibold">{title}</h2>
+
+      {hasLink && (
+        <Button
+          variant="ghost"
+          className="h-fit p-0 text-primary hover:bg-transparent"
+          asChild
+        >
+          <Link href={link} passHref>
+            See all
+            <ChevronRightIcon size={16} />
+          </Link>
+        </Button>
+      )}
     </div>
   );
-
-  return content;
 }
